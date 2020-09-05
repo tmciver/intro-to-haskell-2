b4044f8bd4f104f7df8e8e854ba9af9193994776
Tim McIver <tim@timmciver.com>
2020-09-05 16:58:00 -0400

    Use infix `mod`.

--- code/haskell/FizzBuzz.hs
+++ code/haskell/FizzBuzz.hs
@@ -1,8 +1,8 @@
 main = putStr (unlines (map f [1..100]))
-  where f i = if mod i 3 == 0 && mod i 5 == 0
+  where f i = if i `mod` 3 == 0 && i `mod` 5 == 0
               then "FizzBuzz"
-              else if mod i 3 == 0
+              else if i `mod` 3 == 0
                    then "Fizz"
-                   else if mod i 5 == 0
+                   else if i `mod` 5 == 0
                         then "Buzz"
                         else show i
