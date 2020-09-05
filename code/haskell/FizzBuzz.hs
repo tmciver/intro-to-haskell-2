f3f625e355d2348a88af76f59812de63d8a14531
Tim McIver <tim@timmciver.com>
2020-09-05 17:00:29 -0400

    Rename f to fizzBuzz.

--- code/haskell/FizzBuzz.hs
+++ code/haskell/FizzBuzz.hs
@@ -1,5 +1,5 @@
-main = putStr (unlines (map f [1..100]))
-  where f i = if i `mod` 3 == 0 && i `mod` 5 == 0
+main = putStr (unlines (map fizzBuzz [1..100]))
+  where fizzBuzz i = if i `mod` 3 == 0 && i `mod` 5 == 0
               then "FizzBuzz"
               else if i `mod` 3 == 0
                    then "Fizz"
