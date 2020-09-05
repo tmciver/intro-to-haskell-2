91d85899b9c1174d59e7470c86c69a9ecec80554
Tim McIver <tim@timmciver.com>
2020-09-05 17:01:08 -0400

    Make `fizzBuzz` function a top-level function.

--- code/haskell/FizzBuzz.hs
+++ code/haskell/FizzBuzz.hs
@@ -1,8 +1,9 @@
 main = putStr (unlines (map fizzBuzz [1..100]))
-  where fizzBuzz i = if i `mod` 3 == 0 && i `mod` 5 == 0
-              then "FizzBuzz"
-              else if i `mod` 3 == 0
-                   then "Fizz"
-                   else if i `mod` 5 == 0
-                        then "Buzz"
-                        else show i
+
+fizzBuzz i = if i `mod` 3 == 0 && i `mod` 5 == 0
+             then "FizzBuzz"
+             else if i `mod` 3 == 0
+                  then "Fizz"
+                  else if i `mod` 5 == 0
+                       then "Buzz"
+                       else show i
