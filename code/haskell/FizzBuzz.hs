604ab11be79d444c71c3e9cf35d54c7583386902
Tim McIver <tim@timmciver.com>
2020-09-05 21:38:57 -0400

    Refactor so that explicit check for both mod 3 and 5 is unnecessary.

--- code/haskell/FizzBuzz.hs
+++ code/haskell/FizzBuzz.hs
@@ -1,9 +1,8 @@
 main = putStr (unlines (map fizzBuzz [1..100]))
 
-fizzBuzz i = if i `mod` 3 == 0 && i `mod` 5 == 0
-             then "FizzBuzz"
-             else if i `mod` 3 == 0
-                  then "Fizz"
-                  else if i `mod` 5 == 0
-                       then "Buzz"
-                       else show i
+fizzBuzz i = if null fizzBuzzStr
+             then show i
+             else fizzBuzzStr
+  where fizzBuzzStr =
+          if i `mod` 3 == 0 then "Fizz" else ""
+          ++ if i `mod` 5 == 0 then "Buzz" else ""
